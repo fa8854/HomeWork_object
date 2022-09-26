ff7c17efba441ce1fa307ce6f08796971d098f39
fa8854 <fa8854@yandex.ru>
2022-09-26 09:38:41 +0300

    Домашняя работа по методам

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        homeWork1();
+        homeWork3();
+        homeWork2();
+        homeWork4();
+        homeWork5();
+    }
+    public static void homeWork1(){
+        System.out.println("Задача 1");
+        System.out.println();
+            }
+    public static void homeWork2 (){
+        System.out.println("Задача 2");
+        System.out.println();
+    }
+    public static void homeWork3 (){
+        System.out.println("Задача 3");
+        System.out.println();
+    }
+    public static void homeWork4 (){
+        System.out.println("Задача 4");
+        System.out.println();
+    }
+    public static void homeWork5 (){
+        System.out.println("Задача 5");
+        System.out.println();
+    }
+}
