382d0232edc51ffc29387e1b5013ba6ad885f612
fa8854 <fa8854@yandex.ru>
2022-09-28 10:54:17 +0300

    Домашняя работа по методам

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,60 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        homeWork1();
-        homeWork3();
-        homeWork2();
-        homeWork4();
-        homeWork5();
+        homeWork1(2011);
+        homeWork2(1);
+        homeWork3(23);
     }
-    public static void homeWork1(){
+    public static void homeWork1(int year){
         System.out.println("Задача 1");
+        if (((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0) {
+            System.out.println(year + " Год висакосный");
+        } else {
+            System.out.println(year + " Год не висакосный");
+        }
         System.out.println();
             }
-    public static void homeWork2 (){
+
+    public static int currentYear(){
+        int year = LocalDate.now().getYear();
+        return year;
+    }
+
+    public static void homeWork2 (int clientOS){
         System.out.println("Задача 2");
+        int clientDeviceYear = currentYear();
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке»");
+        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("«Установите версию приложения для iOS по ссылке»");
+        }
+
+        if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("«Установите облегченную версию приложения для Android по ссылке»");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("«Установите версию приложения для Android по ссылке»");
+        }
+
         System.out.println();
     }
-    public static void homeWork3 (){
+
+    public static void homeWork3 ( int deliveryDistance ){
         System.out.println("Задача 3");
+        var deliveryDays = 0;
+        if (deliveryDistance <= 20) {
+            deliveryDays = deliveryDays + 1;
+            System.out.println("Потребуеться дней : " + deliveryDays);
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+            deliveryDays = deliveryDays + 2;
+            System.out.println("Потребуеться дней : " + deliveryDays);
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            deliveryDays = deliveryDays + 3;
+            System.out.println("Потребуеться дней : " + deliveryDays);
+        }
+
         System.out.println();
     }
-    public static void homeWork4 (){
-        System.out.println("Задача 4");
-        System.out.println();
-    }
-    public static void homeWork5 (){
-        System.out.println("Задача 5");
-        System.out.println();
-    }
+
+
 }
